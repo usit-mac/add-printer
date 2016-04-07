2def563a3ab69239e1aab1fc64311fd6aba07e26
Kidist Seifu Gragne <kidistsg@1x-193-157-253-136.uio.no>
2016-04-07 13:44:51 +0200

    editing six lines

--- add-printer.py
+++ add-printer.py
@@ -5,15 +5,13 @@ import subprocess
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-p','--printer-name',default= "test-printer", help ="Name of a printer")
-parser.add_argument('-v','--device-url',default ="smb://pushprint.uio.no/", help ="Print device URL")
-parser.add_argument('-P','--ppd-path',default = "/Library/Printers/PPDs/Contents/Resource/HP LaserJet 600 M601 M602 M603.gz", help="Path to PPD files")
+parser.add_argument('-p','--printerName',default= "test-printer", help ="Name of a printer")
+parser.add_argument('-P','--ppd_path',default = "/Library/Printers/PPDs/Contents/Resource/HP\ LaserJet\ 600\ M601\ M602\ M603.gz", help="Path to PPD files")
 parser.add_argument('-L','--location',default="usit_1340",help ="Printer location")
 
-args = parser.parser_args()
+args = parser.parse_args()
 
-PATH = args.device-url + args.printer-name
+url = "smb://pushprint.uio.no"
+PATH = url + args.printerName
 
-subprocess.check_call(["lpadmin","-p args.printer-name", "-L args.location", "-v PATH", "-P args.ppd-path", "-L pars.location])
-
-
+subprocess.check_call(["lpadmin","-p", args.printerName, "-L", args.location, "-v", PATH, "-P", args.ppd_path, "-L", args.location])
