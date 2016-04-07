bb06e4547f5483d5c16ccb1b47956fedc4c34d76
Kidist Seifu Gragne <kidistsg@1x-193.157.224.130.uio.no>
2016-04-07 12:10:02 +0200

    editing five lines

--- add-printer.py
+++ add-printer.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-p','--printer-name',default= "test-printer", help ="Name of a printer")
 parser.add_argument('-v','--device-url',default ="smb://pushprint.uio.no/", help ="Print device URL")
-parser.add_argument('-P','--path',default = "/Library/Printers/PPDs/Contents/Resource/HP LaserJet 600 M601 M602 M603.gz", help="Path to PPD files")
-parser.add_argument('-L','--location',help ="Printer location")
+parser.add_argument('-P','--ppd-path',default = "/Library/Printers/PPDs/Contents/Resource/HP LaserJet 600 M601 M602 M603.gz", help="Path to PPD files")
+parser.add_argument('-L','--location',default="usit_1340",help ="Printer location")
+
 args = parser.parser_args()
 
-cursor = db.cursor()
-cursor.execute("lpadmin -p args.printer-name -L args.location -v args.device-url -P args.path")
-db.close()
+PATH = args.device-url + args.printer-name
+
+subprocess.check_call(["lpadmin","-p args.printer-name", "-L args.location", "-v PATH", "-P args.ppd-path", "-L pars.location])
 
 
