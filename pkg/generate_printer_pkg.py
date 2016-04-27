ac7f0eecfcbece102aa0b24b9eb4bb306e632e3c
Kidist Seifu Gragne <kidistsg@1x-193.157.224.234.uio.no>
2016-04-27 11:12:38 +0200

    Path checking and makedirs command edited

--- pkg/generate_printer_pkg.py
+++ pkg/generate_printer_pkg.py
@@ -14,22 +14,20 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
    
-    if not os.path.exists(os.path.join(args.output_directory, 'Makefile-out')):
-	os.makedirs(os.path.join(args.output_directory, 'Makefile-out'))
-	with open('Makefile', 'r') as infile:
-	    with open(join(args.output_directory, 'Makefile-out'), 'w') as ofile:
-	       ofile.write(infile.read().replace('__PPDFILE__', args.ppd))
+    if not os.path.exists(args.output_directory):
+	os.makedirs(args.output_directory)
+	
+    with open('Makefile', 'r') as infile:
+	 with open('Makefile-out', 'w') as ofile:
+	     ofile.write(infile.read().replace('__PPDFILE__', args.ppd))
 
+    with open('postinstall', 'r') as infile:
+	content_str=infile.read()
 
-    if not os.path.exists(os.path.join(args.output_directory, 'postinstall')):
-	os.makedirs(os.path.join(args.output_directory, 'postinstall-out'))
-        with open('postinstall', 'r') as infile:
- 	    content_str=infile.read()
+    content_str = content_str.replace('__NAME__',args.name)	
+    content_str = content_str.replace('__LOCATION__',args.location)
+    content_str = content_str.replace('__PPD__',args.ppd)
 
-        content_str = content_str.replace('__NAME__',args.name)	
-        content_str = content_str.replace('__LOCATION__',args.location)
-        content_str = content_str.replace('__PPD__',args.ppd)
-
-        with open('postinstall-out','w') as ofile:
-	    with open(join(args.output-directory, 'postinstall-out'), 'w') as ofile:
-                ofile.write(content_str)
+    with open('postinstall-out','w') as ofile:
+	with open('postinstall-out', 'w') as ofile:
+            ofile.write(content_str)
