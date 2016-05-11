93a239a56994daf24f2e8622358c1e471dff1894
Benedicte Emilie Brækken <b.e.brakken@usit.uio.no>
2016-05-11 10:09:20 +0200

    Small fixes

--- pkg/generate_printer_pkg.py
+++ pkg/generate_printer_pkg.py
@@ -2,7 +2,7 @@
 import sys
 import argparse
 import os
-from os.path import join
+import os.path as op
 import shutil
 import subprocess
 
@@ -10,44 +10,47 @@ _MAKE = '/usr/bin/make'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument('--name', required=True, help="Name of the printer")
     parser.add_argument('--location', default="No specific loacation",)
     parser.add_argument('--ppd', required=True,help="PPD file") 
     parser.add_argument('--output-directory', required=True,help="Path to PPD file") 
 
     args = parser.parse_args()
-   
+
+    name = args.name.replace(' ', '_').lower()
+
     if not os.path.exists(args.output_directory):
-	os.makedirs(args.output_directory)
-	
+        os.makedirs(args.output_directory)
+
     # If in same directory, don't copy
-    if not os.path.join('.',args.ppd) == os.path.join(args.output_directory, args.ppd):
-        shutil.copy(args.ppd,args.output_directory)
-	
+    if not args.ppd in os.listdir(args.output_directory):
+        shutil.copy(args.ppd, args.output_directory)
+
     with open('Makefile-template', 'r') as infile:
-	content_str=infile.read()
-	
-    content_str = content_str.replace('__PPDFILE__',args.ppd)
-    content_str = content_str.replace('__ADDPRINTER__',args.name)
+        content_str = infile.read()
+
+    content_str = content_str.replace('__PPDFILE__',
+            op.basename(args.ppd).replace(' ', '\ '))
+    content_str = content_str.replace('__PRINTERNAME__', name)
 
     with open(os.path.join(args.output_directory,'Makefile'), 'w') as ofile:
-	 ofile.write(content_str)
+        ofile.write(content_str)
 
     with open('postinstall-template', 'r') as infile:
-	content_str=infile.read()
+        content_str = infile.read()
 
-    content_str = content_str.replace('__NAME__',args.name)	
-    content_str = content_str.replace('__LOCATION__',args.location)
-    content_str = content_str.replace('__PPD__',args.ppd)
+    content_str = content_str.replace('__NAME__', name)
+    content_str = content_str.replace('__LOCATION__', args.location)
+    content_str = content_str.replace('__PPD__', args.ppd)
 
     with open(os.path.join(args.output_directory,'postinstall'), 'w') as ofile:
         ofile.write(content_str)
 
-    subprocess.check_call([_MAKE, "pkg", "-C", args.output_directory])
+    try:
+        subprocess.check_call(['sudo', _MAKE, "pkg", "-C", args.output_directory])
+    except Exception as e:
+        print "Make failed with\n  %s" % str(e)
 
-    os.remove(os.path.join(args.output_directory,'Makefile'))
-    os.remove(os.path.join(args.output_directory,'postinstall'))
-
-    if not os.path.join('.',args.ppd) == os.path.join(args.output_directory, args.ppd):
-        os.remove(os.path.join(args.output_directory,args.ppd))
+    os.remove(op.join(args.output_directory,'Makefile'))
+    os.remove(op.join(args.output_directory,'postinstall'))
