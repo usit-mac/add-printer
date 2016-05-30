e104c961c0780a45486511c4e6f29deb65a84e2b
Benedicte Emilie Brækken <b.e.brakken@usit.uio.no>
2016-05-30 12:00:17 +0200

    Re-made into package

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from setuptools import setup
+
+name='printerpkg'
+description='Utility for creating pkgs to install os x printers.'
+version='1'
+
+author='Benedicte Emilie Brækken'
+author_email='b.e.brakken@usit.uio.no'
+
+packages=['printerpkg', 'printerpkg.resources']
+install_requires=[]
+package_data={'': ['Makefile-template', 'postinstall-template']}
+
+scripts=['bin/gen-printer-pkg']
+
+setup(name=name,
+      version=version,
+      description=description,
+      author=author,
+      author_email=author_email,
+      packages=packages,
+      install_requires=install_requires,
+      scripts=scripts,
+      package_data=package_data)
